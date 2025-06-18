c8ffc0dffb3ef9d4c5734328802d5b76e2eee80c
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-06-18 12:15:54 +0530

    Auth Fixed

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function AuthLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -8,30 +8,34 @@ export default function Login() {
   const router = useRouter();
 
   async function handleSubmit(e) {
-  e.preventDefault();
-  const res = await fetch("/api/login", {
-    method: "POST",
-    body: JSON.stringify(form),
-    headers: {
-      "Content-Type": "application/json"
-    },
-    credentials: "include"
-  });
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    });
 
-  if (res.ok) {
-    alert("Login Success!");
-    router.push("/");
-  } else {
-    const error = await res.json();
-    alert(`Login failed: ${error.error || "Unknown error"}`);
+    if (res.ok) {
+      alert("Login Success!");
+      // router.push("/");
+      router.push('/').then(() => window.location.reload());
+    } else {
+      const error = await res.json();
+      alert(`Login failed: ${error.error || "Unknown error"}`);
+    }
   }
-}
 
   return (
-    <form onSubmit={handleSubmit} className="w-full h-screen flex flex-col gap-4 justify-center items-center">
-      <input placeholder="Email" onChange={e => setForm({ ...form, Email: e.target.value })} className="input"/>
-      <input type="password" placeholder="Password" onChange={e => setForm({ ...form, Password: e.target.value })} className="input"/>
-      <button type="submit" className="border hover:bg-black/20 px-3 py-1">Login</button>
-    </form>
+    <div className="w-full h-screen flex justify-center items-center flex-col">
+      <form onSubmit={handleSubmit} className="w-fit h-fit border flex flex-col gap-4 justify-center items-center px-10 py-6 rounded-xl">
+        <h2 className="text-3xl mb-10">Admin Login</h2>
+        <input placeholder="Email" onChange={e => setForm({ ...form, Email: e.target.value })} className="input w-[350px]" required />
+        <input type="password" placeholder="Password" onChange={e => setForm({ ...form, Password: e.target.value })} className="input input w-[350px]" required />
+        <button type="submit" className="border hover:bg-black/20 px-3 py-1 rounded-lg mt-4">Login</button>
+      </form>
+    </div>
   );
 }
